195e7883610b618c32438a3f19b67f6e9f325ee5
nathfreire <nath.efreire@gmail.com>
2026-03-16 20:05:39 +0100

    Realizada la petición de datos y guardado en csv

--- scripts_python/extract_api.py
+++ scripts_python/extract_api.py
@@ -1,8 +1,31 @@
 # Fase 1: Extracción de datos desde API
 # Este script extrae tendencias de GitHub
 
-def main():
+""" def main():
     pass
 
 if __name__ == "__main__":
     main()
+"""
+
+import requests
+import pandas as pd
+
+# 1. Pedir datos a la API (Como llamar por teléfono a GitHub)
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+response = requests.get(url)
+data = response.json()['items']
+
+# 2. Seleccionar lo que nos sirve (Limpieza manual básica)
+repos_list = []
+for repo in data:
+    repos_list.append({
+        'name': repo['name'],
+        'stars': repo['stargazers_count'],
+        'language': repo['language']
+    })
+
+# 3. Guardar en el "Disco Duro"
+df = pd.DataFrame(repos_list)
+df.to_csv("data/repos_python.csv", index=False)
+print("¡Archivo guardado en la carpeta data!")
