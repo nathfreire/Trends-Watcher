f7e68ab4395817a986a93c8fba397fc54c2ca92b
nathfreire <nath.efreire@gmail.com>
2026-05-21 16:14:13 +0200

    cambiado para que lea otro parquet

--- scripts_python/read_parquet.py
+++ scripts_python/read_parquet.py
@@ -3,5 +3,5 @@
 import pandas as pd
 
 # Cambia la ruta por la de tu archivo
-df = pd.read_parquet('output/resultado_parquet/part-00000-b550fd2f-f410-4204-9456-d0ef0ad68721-c000.snappy.parquet')
+df = pd.read_parquet('output/resultado_parquet/part-00000-25057574-bf4f-481c-8c1a-68c658cbfea7-c000.snappy.parquet')
 print(df.head())
