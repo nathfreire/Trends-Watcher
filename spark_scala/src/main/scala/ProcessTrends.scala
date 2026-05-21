9b648f5e05c2b7926aae8e7d9db529934d3b2df1
nathfreire <nath.efreire@gmail.com>
2026-05-21 16:20:04 +0200

    Para que se aguarde como Delta

--- spark_scala/src/main/scala/ProcessTrends.scala
+++ spark_scala/src/main/scala/ProcessTrends.scala
@@ -6,12 +6,18 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.IntegerType
+import io.delta.tables._ // Importamos las librerías de Delta Lake
 
 object ProcessTrends {
   def main(args: Array[String]): Unit = {
     
     val spark = SparkSession.builder()
       .appName("GitHubTrendsProcessor")
+
+      // Configuraciones recomendadas para asegurar el soporte de Delta en Spark
+      .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+      .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+ 
       // No ponemos .master("local") porque Docker/Spark-Submit ya lo gestionan
       .getOrCreate()
 
@@ -31,15 +37,36 @@ object ProcessTrends {
       .select("name", "stars", "language")
 
     // 3. Escritura (Ruta de salida que mapearemos a tu Windows)
-    val outputPath = "/app/output/resultado_parquet"
+    // en Delta con MERGE
+    val outputPath = "/app/output/bronze/delta_bruto"
 
-    println(s"-> Guardando datos procesados en: $outputPath")
+    println(s"-> Guardando datos procesados en Delta en: $outputPath")
     
-    dfClean.write
-      .mode("overwrite")
-      .parquet(outputPath)
+    // Verificamos si la tabla Delta ya existe para hacer el MERGE
+    if (DeltaTable.isDeltaTable(spark, outputPath)) {
+      val deltaTable = DeltaTable.forPath(spark, outputPath)
 
-    println("✅ ¡Éxito! El archivo Parquet ha sido generado.")
+      // Ejecutamos el MERGE usando 'name' como clave de negocio
+      deltaTable.as("target")
+        .merge(
+          dfClean.as("updates"),
+          "target.name = updates.name"
+        )
+        .whenMatched().updateAll() // Si el repo ya existe, actualiza sus estrellas/idioma
+        .whenNotMatched().insertAll() // Si es nuevo, lo inserta
+        .execute()
+
+      println("✅ ¡Éxito! Datos integrados mediante MERGE en la tabla Delta.")
+    } else {
+      // Si la tabla no existe (primera ejecución), la creamos escribiendo el dataframe directamente
+      dfClean.write
+        .format("delta")
+        .mode("overwrite")
+        .save(outputPath)
+
+      println("🆕 Tabla Delta original no encontrada. Se ha creado y cargado por primera vez Delta.")
+    }
+    
     
     spark.stop()
   }
