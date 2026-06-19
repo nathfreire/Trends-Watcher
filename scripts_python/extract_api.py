50eff34dbfdc9c220cdfe0624c9a7a7b557322ee
nathfreire <nath.efreire@gmail.com>
2026-06-19 14:46:22 +0200

    Cmbiado el archivo .py que llama a la API para traer los datos. La modificación es que añadí más campos, claves, al parseo

--- scripts_python/extract_api.py
+++ scripts_python/extract_api.py
@@ -18,15 +18,30 @@ response = requests.get(url)
 data = response.json()['items']
 
 # 2. Seleccionar lo que nos sirve (Limpieza manual básica)
-repos_list = []
+parsed_data = []
 for repo in data:
-    repos_list.append({
-        'name': repo['name'],
-        'stars': repo['stargazers_count'],
-        'language': repo['language']
-    })
+    parsed_data.append({
+            # --- Campos básicos ---
+            "id": repo.get("id"),
+            "name": repo.get("name"),
+            
+            # --- NUEVOS CAMPOS---
+            "owner_login": repo.get("owner", {}).get("login"), # Datos del dueño (anidado)
+            "description": repo.get("description"),
+            "created_at": repo.get("created_at"),              # Ideal para transformaciones en Scala
+            "updated_at": repo.get("updated_at"),
+            "size": repo.get("size"),
+            "stargazers_count": repo.get("stargazers_count"),  # Contador de estrellas
+            "watchers_count": repo.get("watchers_count"), # Diferente a stars, interesante comparar
+            "language": repo.get("language"),
+            "forks_count": repo.get("forks_count"),
+            "open_issues_count": repo.get("open_issues_count"),
+            "license_name": repo.get("license", {}).get("name") if repo.get("license") else None, # Licencia (anidado)
+            "topics": ", ".join(repo.get("topics", []))        # Lista de etiquetas convertida a texto
+        })
+
 
 # 3. Guardar en el "Disco Duro"
-df = pd.DataFrame(repos_list)
+df = pd.DataFrame(parsed_data)
 df.to_csv("data/repos_python.csv", index=False)
 print("¡Archivo guardado en la carpeta data!")
